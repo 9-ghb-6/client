93e0df047e659da3bf96b37cd4c334b910bccf48
9-ghb-6 <huba159@naver.com>
2022-08-24 12:42:42 +0900

    2022-08-24

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,10 @@
-import { Alert } from 'react-bootstrap'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="alert" alert-success>
-      <Alert varaint="success">
-        부트스트랩을 적용한 리액트 애
-      </Alert>
-      <Alert varaint="danger">
-        부트스트랩을 적용한 리액트 애
-      </Alert>
-      <h1>공사중</h1>
+    <div className="alert alert-success">
+      부트스트랩을 적용한 리액트 앱
     </div>
   );
 }
