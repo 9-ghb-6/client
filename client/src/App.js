8ff0fa77111e671e50eee0aeb9d3ca62097c2cc9
9-ghb-6 <huba159@naver.com>
2022-08-24 11:49:41 +0900

    2022-08-24

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="jumbotron">
+      <Alert varaint="success">
+        부트스트랩을 적용한 리액트 애
+      </Alert>
+      <h1>공사중</h1>
+    </div>
+  );
+}
+
+export default App;
