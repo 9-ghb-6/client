fabe786d32d095103464ca24d2c18bd45e1886ce
9-ghb-6 <huba159@naver.com>
2022-08-24 16:56:23 +0900

    2022-08-24

--- server/app.js
+++ server/app.js
@@ -8,13 +8,13 @@ import api from "./route/api.js"
 const app = express();
 
 app.use(cors());
-app.use(morgan("[Server] :method :url :status (:response-time ms | :date[iso]) "));
-app.use(express.urlencoded({extended: true}));
+app.use(morgan(""));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
 app.use("/api", api);
 
-app.listen(8080, ()=>{
+app.listen(8080, () => {
     console.log("[Server] start.");
 });
